0bdc6fbe1d2418f9081a3ca8e30f9eeea1cf1ff7
Jordan <jnewby@backbonepro.com>
2017-04-25 23:16:41 -0600

    button

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-const Button = (props) => {
-    return (
-        <button>{props.buttonText}</button>
-    )
-};
+const Button = ({buttonText}) => (
+    <button>{buttonText}</button>
+);
 
 export default Button;

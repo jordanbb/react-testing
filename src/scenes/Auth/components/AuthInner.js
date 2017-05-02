30d5f652ad705e2de80ff364cf376b569c6a01b9
Jordan <jnewby@backbonepro.com>
2017-05-02 01:01:30 -0600

    renamed button

--- src/scenes/Auth/components/AuthInner.js
+++ src/scenes/Auth/components/AuthInner.js
@@ -1,11 +1,14 @@
 import React from "react";
+import BIQLogoText from "components/icons/BIQLogoText"
 
 const AuthInner = (props) => {
     return (
         <div className="auth-inner-container">
             <div className="auth-inner">
                 <div className="auth-branding-container">
-                    <div className="auth-logo">BLUEIQ</div>
+                    <div className="auth-logo">
+                        <BIQLogoText fillColor="#fff" />
+                    </div>
                 </div>
                 <div className="auth-scenes">
                     {props.children}

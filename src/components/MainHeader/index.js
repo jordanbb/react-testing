d84e12ef6a6c3878f534b90fbeb4a8b43423d21a
Jordan <jnewby@backbonepro.com>
2017-06-02 17:59:53 -0600

    working on main header

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,1 +1,16 @@
 import React from "react";
+import BIQLogoIcon from "components/icons/BIQLogoText";
+import PropTypes from "prop-types";
+import styles from "./MainHeader.scss";
+
+const MainHeader = props => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.inside}>
+                <BIQLogoIcon width="143px" height="29px" />
+            </div>
+        </header>
+    )
+};
+
+export default MainHeader;
--- src/scenes/Auth/index.js
+++ src/scenes/Auth/index.js
@@ -1,14 +1,15 @@
 import React from "react";
+import MainHeader from "components/MainHeader";
 import Login from "./scenes/Login"
 import AuthInner from './components/AuthInner';
-import styles from './auth.scss';
+//import styles from './auth.scss';
 import cx from 'classnames';
 
 class Auth extends React.Component {
     render(){
-        let className = cx(styles.right);
         return (
-            <div className={className}>
+            <div>
+                <MainHeader />
             </div>
         )
     }
